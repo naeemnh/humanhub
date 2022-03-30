1b8bf37e6ef631dfa268c098aa4afe865efabd44
Naeem Hussain <naeemnh@outlook.com>
2022-03-30 23:49:42 +0400

    Posts Page

---
+++ home.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * Template Name: Posts Page
+ * Template Post Type: page
+ */
+
+get_header();
+if (have_posts()) :
+  get_template_part('template-parts/part', 'breadcrumb'); ?>
+  <!-- Begin Main Content Area -->
+  <main class="main-content">
+    <!-- Begin News List Area -->
+    <div class="news-area news-list_area">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 order-2">
+            <?php get_sidebar('post'); ?>
+          </div>
+          <div class="col-lg-8 order-1">
+            <div class="row">
+              <?php
+              while (have_posts()) : the_post();
+              ?>
+                <div class="col-lg-12">
+                  <div class="news-topic">
+                    <div class="inner-topic">
+                      <h2 class="heading mb-0"><a href="news-list.html"><?php echo get_the_category()[0]->name; ?></a></h2>
+                      <h3 class="headline mb-0"><a href="single-post.html"><?php the_title() ?></a></h3>
+                      <div class="entry-meta">
+                        <ul>
+                          <li>
+                            <a href="javascript:void(0)">
+                              <i class="ion-ios-person"></i>
+                              <?php the_author() ?>
+                            </a>
+                          </li>
+                          <!-- <li>
+                              <a href="javascript:void(0)">
+                                <i class="ion-eye"></i>
+                                <span>342 views</span>
+                              </a>
+                            </li>
+                            <li>
+                              <a href="javascript:void(0)">
+                                <i class="ion-android-favorite-outline"></i>
+                                <span>0 like</span>
+                              </a>
+                            </li> -->
+                        </ul>
+                      </div>
+                      <p class="short-desc mb-0">
+                        <?php the_excerpt() ?>
+                      </p>
+                      <div class="btn-wrap">
+                        <a class="ademy-btn denim-color_btn with-border_radius" href="<?php echo get_the_permalink() ?>">Read more <i class="ion-android-arrow-forward"></i></a>
+                        <!-- <a class="share-post_btn" href="javascript:void(0)">
+                            <i class="ion-android-share-alt"></i>
+                          </a> -->
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              <?php
+              endwhile; ?>
+            </div>
+          </div>
+        </div>
+        <!-- Pagination -->
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="paginatoin-area position-center pb-0">
+              <!-- <ul class="pagination-box">
+                <li class="active"><a href="javascript:void(0)">1</a></li>
+                <li><a href="javascript:void(0)">2</a></li>
+                <li><a href="javascript:void(0)">3</a></li>
+              </ul> -->
+              <?php humanhub_pagination() ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- News List Area End Here -->
+  </main>
+  <!-- Main Content Area End Here -->
+<?php
+endif;
+get_footer();
--- template-parts/part-breadcrumb.php
+++ template-parts/part-breadcrumb.php
@@ -1,5 +1,16 @@
 <?php
-$cur_title = (is_page() || is_single()) ? get_the_title() : ((is_archive()) ? get_the_archive_title() : get_search_query());
+// Long version of line 13
+// $cur_title = '';
+// if (is_page() || is_single()) {
+//     $cur_title = get_the_title();
+// } elseif (is_archive()) {
+//     $cur_title = get_the_archive_title();
+// } elseif (is_home()) {
+//     $cur_title = get_the_title(get_option('page_for_posts', true));
+// } else {
+//     $cur_title = get_search_query();
+// }
+$cur_title = (is_page() || is_single()) ? get_the_title() : ((is_archive()) ? get_the_archive_title() : (is_home() ? get_the_title(get_option('page_for_posts', true)) : get_search_query()));
 $results = (is_search()) ? $args['results'] : null;
 ?>
 <!-- Begin Ademy's Breadcrumb Area -->
@@ -7,7 +18,6 @@ $results = (is_search()) ? $args['results'] : null;
     <div class="container-fluid h-100">
         <div class="breadcrumb-content h-100">
             <h1 class="text-capitalize mb-0">
-
                 <?php echo (is_search()) ? '' . $results . ' Results Found' : $cur_title ?>
             </h1>
             <ul>
