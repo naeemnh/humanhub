0cd75745c718a2d6461059778877711c5f25ff83
Naeem Hussain <naeemnh@outlook.com>
2022-03-31 13:41:44 +0400

    Adding Custom Post Type - Event

--- functions.php
+++ functions.php
@@ -7,10 +7,13 @@
 
 function mehan_theme_setup()
 {
+    // Theme Support
     add_theme_support('post-thumbnails');
     add_theme_support('html5', array('search-form'));
 
-    $labels = array(
+
+    // Custom Post Types
+    $testimonial_labels = array(
         'name'              => _x('Testimonials', 'post type general name'),
         'singular_name'     => _x('Testimonial', 'post type singular name'),
         'add_new'           => _x('Add New', 'testimonial'),
@@ -24,8 +27,8 @@ function mehan_theme_setup()
         'parent_item_colon' => '',
         'menu_name'         => 'Testimonials',
     );
-    $args = array(
-        'labels'            => $labels,
+    $testimonial_args = array(
+        'labels'            => $testimonial_labels,
         'public'            => true,
         'publicly_queryable' => true,
         'show_ui'           => true,
@@ -39,8 +42,40 @@ function mehan_theme_setup()
         'menu_icon'         => 'dashicons-testimonial',
         'supports'          => array('title', 'editor', 'thumbnail', 'excerpt'),
     );
-    register_post_type('testimonial', $args);
+    register_post_type('testimonial', $testimonial_args);
 
+    $event_labels = array(
+        'name'              => _x('Events', 'post type general name'),
+        'singular_name'     => _x('Event', 'post type singular name'),
+        'add_new'           => _x('Add New', 'event'),
+        'add_new_item'      => __('Add New Event'),
+        'edit_item'         => __('Edit Events'),
+        'new_item'          => __('New Event'),
+        'view_item'         => __('View Event'),
+        'search_items'      => __('Search Events'),
+        'not_found'         => __('No events found'),
+        'not_found_in_trash' => __('No events found in Trash'),
+        'parent_item_colon' => '',
+        'menu_name'         => 'Events',
+    );
+    $event_args = array(
+        'labels'            => $event_labels,
+        'public'            => true,
+        'publicly_queryable' => true,
+        'show_ui'           => true,
+        'show_in_menu'      => true,
+        'query_var'         => true,
+        'rewrite'           => true,
+        'capability_type'   => 'post',
+        'has_archive'       => true,
+        'hierarchical'      => true,
+        'menu_position'     => null,
+        'menu_icon'         => 'dashicons-calendar-alt',
+        'supports'          => array('title', 'editor', 'thumbnail', 'excerpt'),
+    );
+    register_post_type('event', $event_args);
+
+    // Navigation Menus
     register_nav_menus(
         array(
             'primary-menu'  => __('Primary Menu'),
