7c9265a36d10b4beba5b8e8d70cc4e337ecdde7e
Naeem Hussain <naeemnh@outlook.com>
2022-03-31 13:39:20 +0400

    Remove commented code

--- home.php
+++ home.php
@@ -70,11 +70,6 @@ if (have_posts()) :
         <div class="row">
           <div class="col-lg-12">
             <div class="paginatoin-area position-center pb-0">
-              <!-- <ul class="pagination-box">
-                <li class="active"><a href="javascript:void(0)">1</a></li>
-                <li><a href="javascript:void(0)">2</a></li>
-                <li><a href="javascript:void(0)">3</a></li>
-              </ul> -->
               <?php humanhub_pagination() ?>
             </div>
           </div>
